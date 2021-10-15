a0164e93ca844522fae3cd4b0c020f713d165b00
Bravehearts19 <leo18111998@gmail.com>
2021-10-15 16:49:07 +0200

    Last change to exercise 1

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-const numeroUtente = prompt("Inserisci un numero");
+const numeroUtente = parseInt(prompt("Inserisci un numero"));
 let numeroUtenteTrasformato;
 
 if (numeroUtente % 2 === 0) {
     numeroUtenteTrasformato = numeroUtente;
     alert(numeroUtenteTrasformato);
+} else if (isNaN(numeroUtente)) {
+    alert("Non hai inserito un numero")
 } else {
-    numeroUtenteTrasformato = parseInt(numeroUtente) + 1;
+    numeroUtenteTrasformato = numeroUtente + 1;
     alert(numeroUtenteTrasformato);
 }
