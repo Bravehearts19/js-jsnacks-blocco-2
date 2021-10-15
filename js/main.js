d805d6d8370f1ae9a762caffa663d3583d2b8d92
Bravehearts19 <leo18111998@gmail.com>
2021-10-15 16:56:19 +0200

    Exercise 2 completed

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-const numeroUtente = parseInt(prompt("Inserisci un numero"));
+/* Esercizio 1 */
+
+/* const numeroUtente = parseInt(prompt("Inserisci un numero"));
 let numeroUtenteTrasformato;
 
 if (numeroUtente % 2 === 0) {
@@ -9,4 +11,24 @@ if (numeroUtente % 2 === 0) {
 } else {
     numeroUtenteTrasformato = numeroUtente + 1;
     alert(numeroUtenteTrasformato);
+} */
+
+/* Esercizio 2 */
+
+/* const array1 = [15, 2, 7];
+const array2 = [5]; */
+
+const array1 = ['ciao'];
+const array2 = ['hola', 'hello'];
+
+for (; array1.length !== array2.length;) {
+    const randomNumber = Math.random * 100;
+    
+    if (array1.length > array2.length) {
+        array2.push(randomNumber)
+    } else {
+        array1.push(randomNumber)
+    }   
 }
+
+console.log(array1.length, array2.length);
